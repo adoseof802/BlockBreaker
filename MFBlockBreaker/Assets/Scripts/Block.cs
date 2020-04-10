a9daf3f66766cc691995a240804f0955e172766e
Mandy Farrugia <54974289+adoseof802@users.noreply.github.com>
2020-04-10 14:44:41 +0200

    Handling collisions with the blocks
    If an object hits the block, the latter should disappear from view via Destroy().

---
+++ MFBlockBreaker/Assets/Scripts/Block.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy the game object to which the script is attached. (in this case, the block)
+        Destroy(this.gameObject);
+    }
+}

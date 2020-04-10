4746cd1a7740fc1b30f83fe04ab0ff0b9e33e3ea
Mandy Farrugia <54974289+adoseof802@users.noreply.github.com>
2020-04-10 12:41:57 +0200

    Changed from Text to TextMeshPro
    I will now move on to moving objects with the mouse.

---
+++ MFBlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //This method runs only once when the game launches.
+    void Start()
+    {
+        
+    }
+
+    /* Update runs continuously per frame. 
+     * If the game is running at 60 frames per second, 
+     * this function runs 60 times per second. */
+    void Update()
+    {
+    }
+}

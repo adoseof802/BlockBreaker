dc8cd9f65f531765c0e1e55800bafb1c5b997cba
Mandy Farrugia <54974289+adoseof802@users.noreply.github.com>
2020-04-10 21:54:34 +0200

    Text control displays the updated scores as expected.
    The game status script updates the score and text control as expected.

--- MFBlockBreaker/Assets/Scripts/GameStatus.cs
+++ MFBlockBreaker/Assets/Scripts/GameStatus.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//To be able to use TextMeshProUGUI.
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
@@ -9,21 +11,30 @@ public class GameStatus : MonoBehaviour
     //We will add 10 points for each block destroyed.
     [SerializeField] int pointsPerBlockDestroyed = 10;
 
+    //The text control storing the text score.
+    [SerializeField] TextMeshProUGUI scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        /* Update the scoreText with the current score,
+         * initially it is set to 0. 
+         * Use the TextMeshProUGUI to update the score. */
+        scoreText = FindObjectOfType<TextMeshProUGUI>();
+        //Update the score.
+        scoreText.text = $"Score: {currentScore}";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void AddToScore() 
     {
         //Add the score by 10 points.
         currentScore += pointsPerBlockDestroyed;
+        //Update the score after points have been added.
+        scoreText.text = $"Score: {currentScore}";
     }
 }

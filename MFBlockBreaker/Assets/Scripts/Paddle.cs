848d4eae8b0f166d37af1e1e48fc5d21b4c77c3f
Mandy Farrugia <54974289+adoseof802@users.noreply.github.com>
2020-04-10 13:30:43 +0200

    Changing the horizontal position of the paddle
    The value of the screen width in units is 16. I have retrieved the horizontal mouse position using the horizontal mouse position divided by the screen width multiplied by the screen width in units. I have also created a Vector2 struct to change the horizontal position of the paddle using the horizontal mouse position whilst maintaining the vertical mouse position.

--- MFBlockBreaker/Assets/Scripts/Paddle.cs
+++ MFBlockBreaker/Assets/Scripts/Paddle.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    /* 16 is the world unit in which the mouse is (the camera is 16 units wide.) 
+     * You can find this out by hovering over to the far right side of the screen. */
+    [SerializeField] float screenWidthInUnits = 16f;
+    //SerializeField displays variables in the Unity Editor, the values can change as well.
+
+    //Values to limit the paddle movement.
+    [SerializeField] float minimumX = 1f;
+    [SerializeField] float minimumY = 15f;
+
     //This method runs only once when the game launches.
     void Start()
     {
-        
+        //Determine the position of the mouse to track movement.
+
+        //Get both x and y position of the mouse.
+        print(Input.mousePosition);
+
+        //Get the x and y position individually.
+        print($"x: {Input.mousePosition.x} y: {Input.mousePosition.y}");
     }
 
     /* Update runs continuously per frame. 
@@ -15,7 +30,20 @@ public class Paddle : MonoBehaviour
      * this function runs 60 times per second. */
     void Update()
     {
-        //Determine the position of the mouse to track movement.
-        print(Input.mousePosition);
+        /* For the width of the screen, we get the x (horizontal) position of the mouse. 
+         * Input.mousePosition.x / Screen.width shall give us a value between 0 and 1. */
+
+        //width of screen in pixels (px)
+        float horizontalMousePosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        //Move the paddle horizontally (of course, by using the x-axis)
+        Vector2 paddlePosition = new Vector2(horizontalMousePosition, transform.position.y); //x and y
+
+        /* transform.position.x - current x position
+         * transform.position.y - current y position */
+
+        /* transform.position changes the position of the game object. 
+         * It's as though you're changing the positions from Transform in Unity Editor. */
+        transform.position = paddlePosition;
     }
 }

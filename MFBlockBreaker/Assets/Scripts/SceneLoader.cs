e58a2f585ffcd6213b1b242584bd929f7db354c7
Mandy Farrugia <54974289+adoseof802@users.noreply.github.com>
2020-04-10 16:03:44 +0200

    Script to handle scene management
    I have also handled platform dependent compatibility in case build fails because of EditorApplication.isPlaying.

---
+++ MFBlockBreaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName) 
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame() 
+    {
+        /* Platform dependent compilation,
+         * build will fail if EditorApplication.isPlaying is used for deployment. */
+
+        //If the game is being played on the Unity Editor.
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+        #endif
+
+        //If the game is being played on the Microsoft Windows operating system.
+        #if UNITY_STANDALONE_WIN
+            Application.Quit();
+        #endif
+    }
+}

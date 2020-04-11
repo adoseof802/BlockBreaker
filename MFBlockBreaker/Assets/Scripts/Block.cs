84e753d3d6558eb23159803ec8d8765590077d3f
Mandy Farrugia <54974289+adoseof802@users.noreply.github.com>
2020-04-11 13:44:18 +0200

    Particle effects on each block destroyed
    The particle effects are shown every time a block is destroyed.

--- MFBlockBreaker/Assets/Scripts/Block.cs
+++ MFBlockBreaker/Assets/Scripts/Block.cs
@@ -10,6 +10,9 @@ public class Block : MonoBehaviour
     //Link Level class to this class.
     Level level;
 
+    //Attach the sparkles effect.
+    [SerializeField] GameObject blockSparklesVFX;
+
     void Start()
     {
         //Find object of type Level and save it to level object so we can access the method.
@@ -37,10 +40,25 @@ public class Block : MonoBehaviour
          * The sound will play at the Camera's position. */
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
 
+        //Call the method for the effects.
+        TriggerSparklesVFX();
+
         //Destroy the game object to which the script is attached. (in this case, the block)
         Destroy(this.gameObject);
 
         //Decrement breakableBlocks by 1 until there are no more blocks.
         level.BlockDestroyCount();
+    }
+
+    private void TriggerSparklesVFX() 
+    {
+        //Create the particle effects at the block's current position and rotation.
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+
+        /* Everytime a particle is created, it is cloned and left there 
+         * so we will need to destroy it after some time. */
+
+        //The second parameter is the amount of time to delay before destroying the object.
+        Destroy(sparkles, 1f);
     }
 }
